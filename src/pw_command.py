303fbcc76479e465c790e3094617ab89f1a90c83
DavidKuda <david.kuda@pm.me>
2021-03-03 23:03:30 +0100

    refactor: Use a dictionary for the help texts

--- src/pw_command.py
+++ src/pw_command.py
@@ -21,14 +21,21 @@ The creds.json file should look like this:
 import argparse
 from pw_config import CREDS_DIR, CREDS_FILE_PATH
 import pw_client
+HELP_TEXT = {
+        'get_password': 'Get a password. Pass entity as argument.',
+        'all_sections': 'Print all available sections.',
+        'section': 'Pass a section to print all entities of that section.',
+        'generate_random_pw': 'Print a randomly generated password and add it to your clipboard.'
+    }
 
 
 def main():
     pw = pw_client.PasswordClient(CREDS_DIR, CREDS_FILE_PATH)
-    parser = argparse.ArgumentParser()
-    parser.add_argument('input', type=str)
-    parser.add_argument('-a', '--availability', action='store_true')
-    parser.add_argument('-s', '--section', type=str)
+    parser = argparse.ArgumentParser(description='Manage your passwords from the terminal.')
+    parser.add_argument('-g', '--get_password', type=str, help=HELP_TEXT['get_password'])
+    parser.add_argument('-as', '--all_sections', action='store_true', help=HELP_TEXT['all_sections'])
+    parser.add_argument('-s', '--section', type=str, help=HELP_TEXT['section'])
+    parser.add_argument('-r', '--generate_random_pw', action='store_true', help=HELP_TEXT['generate_random_pw'])
     args = parser.parse_args()
 
     if args.availability:

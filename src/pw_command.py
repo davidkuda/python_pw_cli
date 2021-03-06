9969aaac54cba89ecb24041b47149a05a7bb2525
DavidKuda <david.kuda@pm.me>
2021-03-06 19:26:16 +0100

    refactor: Reformat indentation and other things regarding style

--- src/pw_command.py
+++ src/pw_command.py
@@ -20,28 +20,28 @@ from pw_config import CREDS_DIR, CREDS_FILE_PATH
 import pw_client
 import pyperclip
 
-
 HELP_TEXT = {
-        'input': 'Name of entity that holds the password.',
-        'get_password_from_section': 'Get a password from a specified section. "pw -ga <entity> <section>',
-        'all_sections': 'Print all available sections.',
-        'section': 'Pass a section to print all entities of that section.',
-        'add_new_password': 'Pass an entity as arg and add a new password to the json file.',
-        'generate_random_pw': 'Print a randomly generated password and add it to your clipboard.'
-    }
+    'input': 'Name of entity that holds the password.',
+    'get_password_from_section': 'Get a password from a specified section. "pw -ga <entity> <section>',
+    'all_sections': 'Print all available sections.',
+    'section': 'Pass a section to print all entities of that section.',
+    'add_new_password': 'Pass an entity as arg and add a new password to the json file.',
+    'generate_random_pw': 'Print a randomly generated password and add it to your clipboard.',
+}
 
 
 def main():
-    pw = pw_client.PasswordClient(CREDS_DIR, CREDS_FILE_PATH)
     parser = argparse.ArgumentParser(description='Manage your passwords from the terminal.')
     parser.add_argument('input', type=str, help=HELP_TEXT['input'], nargs='?')
     parser.add_argument('-gs', '--get_password_from_section', type=str, nargs=2, help='')
     parser.add_argument('-as', '--all_sections', action='store_true', help=HELP_TEXT['all_sections'])
-    parser.add_argument('-s', '--section', type=str, help=HELP_TEXT['section'])
-    parser.add_argument('-n', '--add_new_password', type=str, help=HELP_TEXT['add_new_password'])
+    parser.add_argument('-s', '--print_section', type=str, help=HELP_TEXT['section'])
     parser.add_argument('-r', '--generate_random_pw', action='store_true', help=HELP_TEXT['generate_random_pw'])
+    parser.add_argument('-n', '--add_new_password', type=str, help=HELP_TEXT['add_new_password'])
+
     args = parser.parse_args()
 
+    pw = pw_client.PasswordClient(CREDS_DIR, CREDS_FILE_PATH)
 
     if args.get_password_from_section:
         entity = args.get_password_from_section[0]
@@ -66,7 +66,7 @@ def main():
         return
 
     if args.input is None:
-        return print('Nothing happened. No args passed after pw command.')
+        return print('Nothing happened. No flags used. No args passed after pw command.')
 
     return pw.get_pw(args.input)
 

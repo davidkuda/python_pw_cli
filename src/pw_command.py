ecc93404e05fd6509e9cff6284dfd46ae9443811
DavidKuda <david.kuda@pm.me>
2021-03-07 22:38:52 +0100

    docs: Improve help texts

--- src/pw_command.py
+++ src/pw_command.py
@@ -27,14 +27,14 @@ HELP_TEXT = {
         '''Pass a section to print all entities of that section.
         The -s flag can be used together with other functions:
           Use it with "-add_new_password" to write pw to a specific section:
-            "pw -n GitHub -s dev
+            "pw -n GitHub -s dev" -- Writes a new random password for "GitHub" to the section "dev";
           Use it with an argument without a flag to get a password from a specific section:
             "pw -s dev GitHub" -- Gets password for "GitHub" from the section "dev"''',
     'add_new_password': 'Pass an entity as arg and add a new password to the json file.',
     'generate_random_pw': 'Print a randomly generated password and add it to your clipboard.',
     'remove':
-        '''Delete a password from the creds file. Combine together with -s 
-        if the password you want to delete is in another section than "main".
+        '''Delete a password from the creds file. Combine together with "-s" 
+        if the password you want to delete is in an other section than "main".
         Example: "pw -d GitHub -s dev" -> Remove the password for GitHub
         from the section "dev".'''
 }

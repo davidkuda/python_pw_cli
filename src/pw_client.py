a204826b78036449a3e8143cd9800eae3ca307bc
David Kuda <david.kuda@pm.me>
2021-09-06 16:25:35 +0200

    feat: Set option "no_special_characters" with "-rn" (for random password)

--- src/pw_client.py
+++ src/pw_client.py
@@ -44,7 +44,7 @@ class PasswordClient:
         """Generates a random password and returns it as a string."""
         digits = string.digits
         letters = string.ascii_letters
-        punctuation = r"!#$%&()*+:,-./;<=>?@[]^_{|}~" if special_characters else None
+        punctuation = r"!#$%&()*+:,-./;<=>?@[]^_{|}~" if special_characters else ''
         characters = digits + letters + punctuation
         random_password = ''.join(random.choice(characters) for i in range(password_length))
         return random_password
--- src/pw_command.py
+++ src/pw_command.py
@@ -40,8 +40,10 @@ def parse_args():
     parser.add_argument('-e', '--expressive', action='store_true')
     parser.add_argument('-as', '--all_sections', action='store_true', help=HELP_TEXT['all_sections'])
     parser.add_argument('-s', '--section', type=str, help=HELP_TEXT['section'])
+
     parser.add_argument('-r', '--generate_random_pw', action='store_true', help=HELP_TEXT['generate_random_pw'])
     parser.add_argument('-rl', '--random_password_length', type=int, default=42)
+    parser.add_argument('-rn', '--no_special_characters', action='store_false')
 
     parser.add_argument('-n', '--new_secrets_data', type=str, help=HELP_TEXT['add_new_password'])
     parser.add_argument('-pw', '--set_password', type=str, help=HELP_TEXT['set_password'])
@@ -84,7 +86,8 @@ def main():
         if args.set_password:
             new_password = args.set_password
         else:
-            new_password = pw.generate_random_password(password_length=args.random_password_length)
+            new_password = pw.generate_random_password(password_length=args.random_password_length,
+                                                       special_characters=args.no_special_characters)
         encrypted_password = crypto.encrypt(new_password)
         secrets_data['password'] = encrypted_password
 
@@ -128,16 +131,31 @@ def main():
         except KeyError:
             return pw.create_section(args.section)
 
+    if args.no_special_characters is False:
+        # "args.no_special_characters" stores False. Thus, if passed, it will be falsey.
+        # A good user experience is to avoid passing "pw -r -rn" and just enable "pw -rn"
+        # to generate a new random password without special characters.
+        # Thus, "-rn" will activate "-r".
+        args.generate_random_pw = True
+
+    if args.random_password_length != 42:
+        # Same as above with "args.no_special_characters": This control flow is designed to
+        # improve the user experience. Instead of using "pw -r -rl 20" for a random pw with
+        # a length of 20, we can instead just use "pw -rl 20".
+        args.generate_random_pw = True
+
     # TODO: Move function "generate_random_pw" from pw_client to utils
     if args.generate_random_pw:
         if args.entity is not None:
-            random_pw = pw.generate_random_password(password_length=args.random_password_length)
+            random_pw = pw.generate_random_password(password_length=args.random_password_length,
+                                                    special_characters=args.no_special_characters)
         else:
-            random_pw = pw.generate_random_password(password_length=args.random_password_length)
+            random_pw = pw.generate_random_password(password_length=args.random_password_length,
+                                                    special_characters=args.no_special_characters)
         pyperclip.copy(random_pw)
         print('The random password has been copied into your clipboard.')
         print('')
-        return random_pw
+        return True
 
     if args.entity is None:
         print('Nothing happened. No flags used. No args passed after pw command.')

617a05321ece30bec0e46e751e488568f14df29a
David Kuda <david.kuda@pm.me>
2022-02-22 23:07:27 +0100

    Test clipboard value

--- tests/unit/test_pw_cli.py
+++ tests/unit/test_pw_cli.py
@@ -1,36 +1,36 @@
+import pyperclip
+
 from pw.pw_cli import PasswordCommand
 from pw.pw_config import get_test_config
 
 
 c1 = "-s main"
 c2 = "-n pytest -pw refactoring"
-c3 = "-n guitar -kwargs 'brand=fender,model=stratocaster,string_gauge=0.10,amp=princeton_reverb'"
+c3 = "-n guitar -kwargs brand=fender,model=stratocaster,string_gauge=0.10,amp=princeton_reverb"
 c4 = "-f guit -e"
 c5 = "-n kafka -pw stream_processing -s test"
 
 
 def test_get_password():
-    args = "guitar".split()
-    pw = PasswordCommand.main(args, get_test_config())
-    assert pw == "pink_floyd"
+    _test_clipboard("guitar", "pink_floyd")
 
 
 def test_get_password_from_section():
-    args = "kafka --section test".split()
-    pw = PasswordCommand.main(args, get_test_config())
-    assert isinstance(pw, str)
+    _test_clipboard("kafka --section test", "stream_processing")
 
 
 def test_get_key():
-    args = "pw guitar --key brand".split()
-    pw = PasswordCommand.main(args, get_test_config())
-    assert isinstance(pw, str)
+    _test_clipboard("guitar -k brand", "fender")
 
 
 def test_find():
-    args = "pw -f kafka".split()
-    pw = PasswordCommand.main(args, get_test_config())
-    assert isinstance(pw, str)
+    _test_clipboard("-f kaf", "stream_processing")
+
+
+def _test_clipboard(args: str, expected_value: str):
+    PasswordCommand.main(args.split(), get_test_config())
+    actual_value = pyperclip.paste()
+    assert actual_value == expected_value
 
 
 if __name__ == "__main__":

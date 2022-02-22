a150e0306b29923f531609433c14bd33732daf83
David Kuda <david.kuda@pm.me>
2022-02-22 23:09:31 +0100

    Remove lines that are no longer needed

--- tests/unit/test_pw_cli.py
+++ tests/unit/test_pw_cli.py
@@ -4,13 +4,6 @@ from pw.pw_cli import PasswordCommand
 from pw.pw_config import get_test_config
 
 
-c1 = "-s main"
-c2 = "-n pytest -pw refactoring"
-c3 = "-n guitar -kwargs brand=fender,model=stratocaster,string_gauge=0.10,amp=princeton_reverb"
-c4 = "-f guit -e"
-c5 = "-n kafka -pw stream_processing -s test"
-
-
 def test_get_password():
     _test_clipboard("guitar", "pink_floyd")
 
@@ -31,7 +24,3 @@ def _test_clipboard(args: str, expected_value: str):
     PasswordCommand.main(args.split(), get_test_config())
     actual_value = pyperclip.paste()
     assert actual_value == expected_value
-
-
-if __name__ == "__main__":
-    main()
